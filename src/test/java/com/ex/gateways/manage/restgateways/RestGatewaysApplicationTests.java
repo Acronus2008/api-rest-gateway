3224c43c1fc23777628ef9470e0d04e4e376e378
Alejandro <acronus2008@gmail.com>
2020-02-13 12:59:43 -0800

    adding default tests

---
+++ src/test/java/com/ex/gateways/manage/restgateways/RestGatewaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.gateways.manage.restgateways;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestGatewaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
