3f602c1d8aadd330b6cdbe415b92964b8aa4012e
Alejandro <acronus2008@gmail.com>
2020-02-21 16:15:43 -0800

    adding gateway same serial number exception

---
+++ src/main/java/com/ex/gateways/manage/restgateways/error/GatewaySameSerialNumberException.java
@@ -0,0 +1,7 @@
+package com.ex.gateways.manage.restgateways.error;
+
+public class GatewaySameSerialNumberException extends Exception {
+    public GatewaySameSerialNumberException(String message) {
+        super(message);
+    }
+}
